b0385f2d44bf1b769332d07a10b048ecea5fd051
howq <howq@outlook.com>
2017-07-12 02:13:34 +0800

    check WordPress userinfo by java

--- account_bymarx/src/main/java/org/bymarx/account/controller/HelloController.java
+++ account_bymarx/src/main/java/org/bymarx/account/controller/HelloController.java
@@ -26,8 +26,10 @@ public class HelloController {
     @RequestMapping(value = "/login",method = RequestMethod.GET)
     public String login(@RequestParam(value = "username",required = true) String username,
     @RequestParam(value = "pwd",required = true) String pwd){
-        userService.isLogin(username, pwd);
 
-        return "1";
+        if(userService.isLogin(username, pwd)){
+            return "1";
+        }
+        return "0";
     }
 }
